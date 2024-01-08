342decac152819ff62a4ce28be7682c8cb177987
jbonnie <bonnie8623@gmail.com>
2024-01-08 11:40:15 +0900

    create project

---
+++ src/main/java/warmingUp/antifragile/AntifragileApplication.java
@@ -0,0 +1,13 @@
+package warmingUp.antifragile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntifragileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntifragileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/warmingUp/antifragile/member/domain/Member.java
@@ -0,0 +1,15 @@
+package warmingUp.antifragile.member.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Member {
+
+
+}
